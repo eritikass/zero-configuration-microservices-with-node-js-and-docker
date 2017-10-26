9b58814b2444f89cddae07137228e15560dd0d6c
Siim Tiilen <siim.tiilen@ekspressdigital.eu>
2017-10-26 12:27:04 +0200

    example to get all data send to server

--- demo/client.js
+++ demo/client.js
@@ -6,3 +6,6 @@ client.send({
 	type: 'add',
 	name: 'YOUR_NAME_HERE' // << write your name there
 }).then(console.log).catch(console.log);
+
+// get all data send to server
+// client.send({type: 'getall'}).then(console.log).catch(console.log);
